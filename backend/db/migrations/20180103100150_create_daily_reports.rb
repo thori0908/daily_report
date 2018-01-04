b5cf07722a060dc12a15875575f8347069a23088
thori0908 <mr.takumi.pc.0908@gmail.com>
2018-01-04 23:00:07 +0900

    Modify migrations of model

--- backend/db/migrations/20180103100150_create_daily_reports.rb
+++ backend/db/migrations/20180103100150_create_daily_reports.rb
@@ -2,6 +2,7 @@ Hanami::Model.migration do
   change do
     create_table :daily_reports do
       primary_key :id
+      column :submitted_at, DateTime, null: false
 
       column :created_at, DateTime, null: false
       column :updated_at, DateTime, null: false
--- backend/db/migrations/20180103100228_create_tasks.rb
+++ backend/db/migrations/20180103100228_create_tasks.rb
@@ -2,6 +2,9 @@ Hanami::Model.migration do
   change do
     create_table :tasks do
       primary_key :id
+      foreign_key :daily_report_id, :daily_reports, on_delete: :cascade, null: false
+      foreign_key :project_id,      :projects,      on_delete: :cascade, null: false
+      column :time_spent, Integer, null: false
 
       column :created_at, DateTime, null: false
       column :updated_at, DateTime, null: false
